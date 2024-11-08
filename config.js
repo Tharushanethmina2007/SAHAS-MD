4ab1ee0d97cadbedfdeb6a6e8c7c5c4e23b86511
Tharushanethmina2007 <Babatharusha41@gmail.com>
2024-11-08 07:04:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "AP1mTZiS#3GSye10fP6qECetb5VtHpmqKegnL71UlkNLCiyRPDLA",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
